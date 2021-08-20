6a5cd3f875ec1f1e701801e85545642dea8a6bf2
thomas <thomasmonta98@gmail.com>
2021-08-20 23:37:38 +1000

    done with assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size <= 2
+    array.join(' and ')
+  else
+    add_and_to_last_word = "and #{array[-1]}"
+    array.pop
+    array << add_and_to_last_word
+    array.join(', ')
+  end
 end
